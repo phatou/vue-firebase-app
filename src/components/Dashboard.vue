7c406a6f834b04952be1520ffb8c27eadc562e30
phatou <baophat.ipl@gmail.com>
2018-06-09 21:43:04 +0200

    Vue firebase app
    # Initial Setup With Vue CLI
    # State Management With Vuex (and props)
    # Authenticating Routes With vue-router
    # Creating the Log-in and Sign-up View
    # Building the Navigation With Vue Components
    # Handling User State on Page Reload

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="dashboard">
+        <section>
+            <div class="col1">
+                <div class="profile">
+                    <h5>{{ userProfile.name }}</h5>
+                    <p>{{ userProfile.title }}</p>
+                    <div class="create-post">
+                        <p>create a post</p>
+                        <form @submit.prevent>
+                            <textarea></textarea>
+                            <button class="button">post</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col2">
+                <div>
+                    <p class="no-results">There are currently no posts</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
